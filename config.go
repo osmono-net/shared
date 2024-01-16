529b8814d56ba169887f501c9ca999014ead790f
Tom French <hello@orseti.com>
2024-01-16 14:17:16 +0000

    wip

---
+++ config.go
@@ -0,0 +1,14 @@
+package shared
+
+type Config struct {
+	PatchMgmt bool
+	// Time interval between heartbeat requests
+	// back to the C2 server, in seconds.
+	HeartbeatInterval uint
+
+	EnableLogging bool
+	EnableEvents  bool
+	EnableRdp     bool
+	EnablePing    bool
+	EnableSsh     bool
+}
