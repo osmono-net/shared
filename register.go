431e86e780bae47538bcdb2eb8ec78ee70dcb545
Tom French <hello@orseti.com>
2024-01-23 13:32:59 +0000

    vers

--- register.go
+++ register.go
@@ -4,6 +4,14 @@ type RegisterAgentBody struct {
 	HwId       string
 	Name       string
 	OsType     string
-	Version    string
+	Version    WindowsVersion
 	CustomerId string
+}
+
+type WindowsVersion struct {
+	CurrentVersion string
+	ProductName    string
+	MajorVersion   uint64
+	MinorVersion   uint64
+	CurrentBuild   string
 }
