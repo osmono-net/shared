2ded8777f706db20758760d1fe2d43cc3f25c82a
Tom French <hello@orseti.com>
2024-01-24 18:26:55 +0000

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/osmono-net/shared
 
 go 1.19
 
-require github.com/google/uuid v1.5.0
+require github.com/nats-io/nats.go v1.32.0
+
+require (
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
--- nats.go
+++ nats.go
@@ -1,6 +1,31 @@
 package shared
 
+import (
+	"encoding/json"
+
+	"github.com/nats-io/nats.go"
+)
+
 type AgentMessage[T any] struct {
 	Id      string
 	Payload T
+}
+
+func Send(url, channel string, T any) error {
+	nc, err := nats.Connect(url)
+	if err != nil {
+		return err
+	}
+	defer nc.Drain()
+	jb, err := json.Marshal(T)
+	if err != nil {
+		return err
+	}
+
+	err = nc.Publish(channel, jb)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
